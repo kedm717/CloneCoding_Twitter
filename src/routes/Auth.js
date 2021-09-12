3be9dcbe6eff571bc7e75285faa7095850e20d4c
kedm717 <dongchan1583@gmail.com>
2021-09-12 21:23:12 +0900

    구글 및 깃허브 로그인 연동

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import { authService } from "../firebase";
+import { authService , firebaseInstance} from "../firebase";
 
 const Auth = () => {
 
@@ -38,8 +38,18 @@ const Auth = () => {
     };
     const toggleAccount = () => setNewAccount((prev)=> !prev);
 
-    const onSocialClick = (event) =>{
-        console.log(event.target.name);
+    const onSocialClick = async (event) =>{
+       const {
+           target: {name},
+       } = event;
+       let provider;
+       if(name === "google"){
+           provider = new firebaseInstance.auth.GoogleAuthProvider();
+       } else if(name === "github"){
+           provider = new firebaseInstance.auth.GithubAuthProvider();
+       }
+       const data = await authService.signInWithPopup(provider);
+       console.log(data);
     } 
 
     return (     
