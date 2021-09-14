992c8b6c57586b88d75791fdad12bb907fd89ddb
kedm717 <dongchan1583@gmail.com>
2021-09-14 15:10:32 +0900

    실시간 데이터 베이스로 트윗 목록 보여주기 getTweet함수 삭제

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,20 +6,15 @@ const Home = ({userObj}) => {
     const [tweet, setTweet] = useState("")
     const [tweets, setTweets] = useState([]) // 받은 데이터로 게사물 목록을 만들기 위한 useState
 
-    const getTweets = async () =>{
-        const dbTweets = await dbService.collection("tweets").get();
-       dbTweets.forEach((document) => {
-        const tweetObject = {...document.data(), id: document.id};
-    
-        setTweets((prev)=> [tweetObject, ...prev]) // console.log 에서 setTweets 함수로 변경
-       })
-        // console.log(dbTweet)를 출력할시 파이어베이스에서 보낸 스냅샷이 보일건데 스냅샷이 원본을 찍어 보내 데이터가 스냅샷 속에 숨어있기 때문에 , forEach() 함수로 불러와서 원하는 데이터가 
-        // 나올수 있게 출력을 만들어 준 부분!!
-        // console.log를 찍어보니 원하는 데이터 값이 나온다 .  
-    }
     
     useEffect(()=>{
-        getTweets();
+        dbService.collection.apply("tweet").onSnapshot((snapshot)=>{
+            const newArray = snapshot.docs.map((document)=>({
+                id: document.id,
+                ...document.data(),
+            }));
+            setTweet(newArray);
+        })
     },[]);
 
     
